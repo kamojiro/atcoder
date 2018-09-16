c12236745dfd80b98352f80fc914d8ca03a47c4e
kamojiro <tamagoma002@yahoo.co.jp>
2018-09-17 02:04:44 +0900

    CodeFestival2017qualA,ABCを解いた。Dはわかったけど実装がめんどい

--- grand/027/C.py
+++ grand/027/C.py
@@ -3,7 +3,7 @@ N, M = map( int, input().split())
 S = list( input())
 E = [ list( map( int, input().split())) for _ in range(M)]
 V = [ 1 for _ in range(N)]
-Edges = [[] for _ in range(N)]
+Edges = [set() for _ in range(N)]
 A = [0]*N
 B = [0]*N
 d = defaultdict(int)
@@ -11,13 +11,13 @@ for i in range(M):
     a, b = E[i]
     a, b = a-1, b-1
     a, b = min(a,b), max(a,b)
-    Edges[a].append(b)
-    Edges[b].append(a)
-    if d[(a,b)] == 1:
+    Edges[a].add(b) #隣接する頂点をメモする
+    Edges[b].add(a)
+    if d[(a,b)] == 1:#(a,b)という辺を一度入力しれいれば、無視して次のループに進む。
         continue
-    else:
+    else:#一度目の入力をしたことをメモする。
         d[(a,b)] += 1
-    if a == b:
+    if a == b:#ループ(同じ頂点を結ぶ辺)だと、このあとの方法だと、ダブルカウントしてしまうので、別扱い。
         if S[a] == 'A':
             A[a] += 1
         else:
@@ -31,29 +31,29 @@ for i in range(M):
         B[a] += 1
     else:
         A[a] += 1
-T = set([])
+T = set()
 
-for i in range(N):
+for i in range(N):#'A'と’B'のうち、高々1種類しか隣接していない頂点をTに追加する。
     if A[i] == 0 or B[i] == 0:
         T.add(i)
-while T:
-    n = T.pop()
-    V[n] = 0
-    for m in set(Edges[n]):
+
+while T:#'A'と’B'のうち、高々1種類しか隣接していない頂点がなくなるまでループを回す。
+    n = T.pop()#一つ取り出す。
+    V[n] = 0#'A'と’B'のうち、高々1種類しか隣接していない頂点なので、取り除く。
+    for m in Edges[n]:#頂点nが消えると、それに隣接する頂点mに隣接する'A'及び'B'の個数が1だけ減少する。
         if V[m] == 1:
             if S[n] == 'A':
                 A[m] -= 1
-                if A[m] == 0:
+                if A[m] == 0:#隣接する頂点mに隣接する'A'及び'B'の個数が0になると、次に消える頂点になる。
                     T.add(m)
             else:
                 B[m] -= 1
-                if B[m] == 0:
+                if B[m] == 0:#隣接する頂点mに隣接する'A'及び'B'の個数が0になると、次に消える頂点になる。
                     T.add(m)
                 
 if sum(V) == 0:
     print('No')
 else:
     print('Yes')
-            
     
 
