82fcf81947632299b6a1ed98497ca117222d1244
kamojiro <tamagoma002@yahoo.co.jp>
2018-09-25 20:17:21 +0900

    ABC059C問題、苦戦した。思いついたコードが冗長でめんどくさくてやる気が消滅した

--- beginner/059/C.py
+++ beginner/059/C.py
@@ -1,11 +1,74 @@
 n = int( input())
 A = list( map( int, input().split()))
-#+-+-と-+-+の2パターンでやれば良いと思う。前から。
 ansp = 0
 sums = A[0]
-if sums > 0:
-    pass
+if sums == 0:
+    ansp += 1
+    sums += 1
+for i in range(1,n-1):
+    sums += A[i]
+    if i%2 == 1:
+        if sums < 0:
+            pass
+        else:
+            ansp += abs(-1-sums)
+            sums = -1
+    else:
+        if sums > 0:
+            pass
+        else:
+            ansp += abs(1 - sums)
+            sums = 1
+if (n-1)%2 == 0:
+    if A[n-1] > 0:
+        sums += A[n-1]
+        pass
+    else:
+        ansp += abs(1-A[n])
+        sums += 1
 else:
-    
-for i in range(1,n):
-    
+    if A[n-1] < 0:
+        sums += A[n-1]
+        pass
+    else:
+        ansp += abs(-1-A[n-1])
+        sums -= 1
+if sums == 0:
+    ansp += 1
+
+sums = A[0]
+ansm = 0
+if sums == 0:
+    ansm += 1
+    sums -= 1
+for i in range(1,n-1):
+    sums += A[i]
+    if i%2 == 0:
+        if sums < 0:
+            pass
+        else:
+            ansm += abs(-1-sums)
+            sums = -1
+    else:
+        if sums > 0:
+            pass
+        else:
+            ansm += abs(1 - sums)
+            sums = 1
+if (n-1)%2 == 1:
+    if A[n-1] > 0:
+        sums += A[n-1]
+        pass
+    else:
+        ansm += abs(1-A[n-1])
+        sums += 1
+else:
+    if A[n-1] < 0:
+        sums += A[n-1]
+        pass
+    else:
+        ansm += abs(-1-A[n-1])
+        sums -= 1
+if sums == 0:
+    ansm += 1
+print( min(ansp, ansm))
