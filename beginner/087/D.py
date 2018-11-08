f0ced14503ff00c0223b59c01071cef423c401a1
kamojiro <tamagoma002@yahoo.co.jp>
2018-11-08 23:00:55 +0900

    ほぼ写経。頭が働かない日は適度に控えよう。

--- beginner/087/D.py
+++ beginner/087/D.py
@@ -1,4 +1,29 @@
-#(L,R,D)をD>=0となるようにいれかえる
-#最長のグラフの道を求める問題に帰着される
-#ループに注意してwhile
-#連結成分が複数個ある場合どうしよう
+from collections import defaultdict
+N, M = map( int, input().split())
+E = [ [] for _ in range(N)]
+V = [ 0 for _ in range(N)]
+for i in range(M):
+    L, R, D = map( int, input().split())
+    E[L-1].append((R-1,D))
+    V[R-1] += 1
+dist = [-1]*N
+for i in range(N):
+    if V[i]:
+        continue
+    S = [i]
+    dist[i] = 0
+    while S:
+        v = S.pop()
+        for u, d in E[v]:
+            if dist[u] == -1:
+                dist[u] = dist[v] + d
+                S.append(u)
+ans = "Yes"
+for v in range(N):
+    for u, d in E[v]:
+        if dist[v] + d != dist[u]:
+            ans = "No"
+            break
+    if ans == "No":
+        break
+print(ans)
