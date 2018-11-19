f010c3fc3f9dd027f71ad55ea3fc1ab1e2d2b6ed
kamojiro <tamagoma002@yahoo.co.jp>
2018-11-19 20:24:28 +0900

    ABC048C

--- beginner/048/C.py
+++ beginner/048/C.py
@@ -1,20 +1,11 @@
-L = ["dream", "dreamer","erase", "eraser", ""]
-S = input()
-L = len(S)
-ans = "YES"
-while L >= 7:
-    if S[-7:] == "dreamer":
-        L -= 7
-        S = S[:-7]
-    elif S[-6:] == "eraser":
-        L -= 6
-        S = S[:-6]
-    elif S[-5:] == "erase" or S[-5:] == "dream":
-        L -= 5
-        S = S[:-5]
-    else:
-        ans = "NO"
-if S in L:
-    print(ans)
-else:
-    print("NO") 
+N, x = map( int, input().split())
+A = list( map( int, input().split()))
+ans = 0
+if A[0] > x:
+    ans += A[0] - x
+    A[0] = x
+for i in range(N-1):
+    if A[i] + A[i+1] > x:
+        ans += A[i]+A[i+1]-x
+        A[i+1] = x - A[i]
+print(ans)
