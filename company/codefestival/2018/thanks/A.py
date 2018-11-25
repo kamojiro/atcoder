69d4ded952eeccd4b06d165d77a8b829326298c7
kamojiro <tamagoma002@yahoo.co.jp>
2018-11-25 14:18:26 +0900

    はい

--- company/codefestival/2018/thanks/A.py
+++
@@ -1,9 +0,0 @@
-T, A, B, C, D = map( int, input().split())
-ans = 0
-if C <= T:
-    ans += D
-    if A+C <= T:
-        ans += B
-elif A <= T:
-    ans += B
-print(ans)
--- company/codefestival/2018/thanks/D.py
+++
@@ -1,8 +0,0 @@
-S = input()
-ans = 1
-now = S[0]
-for s in S[1:]:
-    if now >= s:
-        ans += 1
-        now = s
-print(ans)
